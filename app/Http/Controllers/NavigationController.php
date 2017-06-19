3f7b027e3c9c33c1e736d3eb863e26c27bcd87fe
Tchouatcha <micheltchouatcha@gmail.com>
2017-06-19 13:43:40 +0100

    Voici une version qui marche , il suffit d'utiliser l'une des route contenu ou navigation pour obtenir l'objet correspondant sous forme d'une chaine de caractère json.  On peut former l'objet à partir de la fonction json_decode()

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -72,28 +72,9 @@ class NavigationController extends Controller
     }
 
 
-    public function lectureContenu ()
+    public function traitement ()
         {
             // on charge le cours
-
-            //$metadata = simplexml_load_file("xmoddledata/metadata.xml");
-
-            // on Vérifie si le cours existe
-
-            /*  @TODO Partie à décommenter lorsque le module de navigation sera intégré au reste de l'application */
-
-            // $nbrCours = $metadata->attributes()->nbrCours;
-            // $isFound = false;
-            // $i = 0;
-            // for ($i = 0; $i < $nbrCours; $i++) {
-            //     if ($metadata->cours[$i]->attributes()->title == $title && $metadata->cours[$i]->attributes()->id == $id) {
-            //         $isFound = true;
-            //         break;
-            //     }
-            // }
-
-            //  if ($isFound) {
-            //$cours = simplexml_load_file("xmoddledata/" . $metadata->cours[$i]->attributes()->id . "_" . $metadata->cours[$i]->attributes()->title . "/description.xml");            
 
             $description = simplexml_load_file('xmoddledata/2_ModeleDeSupport2/description.xml');
             $notions = simplexml_load_file('xmoddledata/2_ModeleDeSupport2/descriptionNotions.xml');
@@ -148,75 +129,29 @@ class NavigationController extends Controller
                 $text_parties .= $text_chapitres;
                 $nav_parties["".$i] = $nav_chapitres;
             }
-            dd($nav_parties);
-//                dd($navigation);
-            dd($description);
-
-            // Construction de la page web
-            $titre = $description->attributes()->title;
-            dd($description);
-//                forearch( $navigation->partie[] as $partie) {
-//                    dd($pantie);
-//                }
-            dd($titre);
-
-
-            // $notions = simplexml_load_file("../../../fichiersdestructuration/descriptionNotions.xml");
-
-
-            // $nbrNotions = $cours->attributes()->nbrNotions;
-            // $notionsConvertis = array();
-            // for ($i = 0; $i < $nbrNotions; $i++) {
-            //     $attributs = $cours->notion[$i]->attributes();
-            //     $style = "";
-            //     if ($attributs['font-weight'] != null) {
-            //         $style .= 'font-weight:' . $attributs['font-weight'] . ";";
-            //     }
-            //     if ($attributs['font-size'] != null) {
-            //         $style .= 'font-size:' . $attributs['font-size'] . ";";
-            //     }
-
-            //     if ($attributs['font-family'] != null) {
-            //         $style .= 'font-family:' . $attributs['font-family'] . ";";
-            //     }
-
-            //     if ($attributs['color'] != null) {
-            //         $style .= 'color:' . $attributs['color'] . ";";
-            //     }
-
-            //     if ($attributs['text-decoration'] != null) {
-            //         $style .= 'text-decoration:' . $attributs['text-decoration'] . ";";
-            //     }
-
-            //     $text = "";
-            //     foreach ($cours->notion[$i]->children() as $child) {
-            //         $text .= $child->asXML();
-            //     }
-            //     $notionHtml = "<div style='";
-            //     $notionHtml .= $style;
-            //     $notionHtml .= "'>";
-            //     $notionHtml .= $text;
-            //     $notionHtml .= "</div>";
-
-
-            //     //dd(strval($text));
-            //     array_push($notionsConvertis, $notionHtml);
-
-            //     }
-            // }else{
-            //     return view("/");
-            // }
-            //$notions = $notionsConvertis;
-            // return response()->json($notions,200);
-
-            //  return "lecture cours";
-            return 0;
+            $resultat = [];
+            $resultat["contenu"] = $text_parties;
+            $resultat["navigation"] = $nav_parties;
+            $resultat = json_encode($resultat);
+            //dd($resultat);
+            return $resultat;
         }
+
+        public function lectureContenu() {
+            $result = json_decode($this->traitement());
+            $result = $result->contenu;
+            return json_encode($result);
+        }
+
+
 
 
     // Fonction qui renvoie la structure de navigation de la page
     public function lectureNavigation() {
-        return "Lecture navigation";
+        $result = json_decode($this->traitement());
+        $result = $result->navigation;
+        return json_encode($result);
+
     }
 
 }
